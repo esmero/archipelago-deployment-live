2b8bc44b730ad514df78020d295f891a31799b0e
Diego Pino Navarro <dpino@metro.org>
2021-06-01 14:28:21 -0400

    Update Settings/Scripts for Live

--- drupal/scripts/archipelago/setup.sh
+++ drupal/scripts/archipelago/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "Adding Drupal 8 basic Configs"
+chmod 755 /var/www/html/web/sites/default/settings.php
 cat <<EOT >> /var/www/html/web/sites/default/settings.php
 \$settings['s3fs.access_key'] = 'minio';
 \$settings['s3fs.secret_key'] = 'minio123';
+\$settings['s3fs.upload_as_private'] = TRUE;
 \$settings['file_private_path'] = '/var/www/html/private';
 ini_set('memory_limit', '512M');
 \$settings['install_profile'] = 'standard';
@@ -13,6 +15,9 @@ if (PHP_SAPI !== 'cli') {
   \$settings['reverse_proxy'] = FALSE;
 }
 EOT
+echo "Updating your web root folder permissions."
+chmod 0666 /var/www/html/web/sites/default/settings.php
+chown -R www-data:www-data /var/www/html/web/sites
 echo "Downloading JQUERY Slider Pips Library for facets" 
 mkdir -p /var/www/html/web/libraries/jquery-ui-slider-pips/dist
 curl -o /var/www/html/web/libraries/jquery-ui-slider-pips/dist/jquery-ui-slider-pips.min.js 'https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.js' 
