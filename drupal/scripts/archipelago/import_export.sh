7d52db5706314a9cd5716611dca718b5b41dbeb2
aksm <amin@metro.org>
2022-09-30 10:36:46 -0400

    Fix unquoted bash variables and inconsistent prompts.

--- drupal/scripts/archipelago/import_export.sh
+++ drupal/scripts/archipelago/import_export.sh
@@ -9,7 +9,7 @@ archipelago_domain_set=0
 json_api_endpoint=""
 
 domain_questions() {
-  if [[ -v ARCHIPELAGO_DOMAIN ]]; then
+  if [[ -v "${ARCHIPELAGO_DOMAIN}" ]]; then
     echo "Is $ARCHIPELAGO_DOMAIN the correct domain? "
     PS3='> '
     select opt in Yes No; do
@@ -25,10 +25,10 @@ domain_questions() {
       esac
     done
   fi
-  if [ $archipelago_domain_set -eq 0 ]
+  if [ "${archipelago_domain_set}" -eq 0 ]
   then
 	  echo -n $'What is the domain? (without protocol or trailing slash, e.g. metro.org or archipelago.nyc or localhost:8001)\n'
-      read archipelago_domain
+      read -p "> " archipelago_domain
   fi
   echo "Is $archipelago_domain HTTPS or HTTP? "
   PS3='> '
@@ -64,12 +64,12 @@ done
 
 unset username
 unset password
-echo -n $'JSON:API Username:\n'
-read username
-prompt="JSON:API Password:"$'\n'
+echo "JSON:API Username:"
+read -p "> " username
+prompt="JSON:API Password: `echo $'\n> '`"
 while IFS= read -p "$prompt" -r -s -n 1 char
 do
-  if [[ $char == $'\0' ]]
+  if [[ "${char}" == $'\0' ]]
   then
     break
   fi
@@ -89,7 +89,7 @@ fi
 echo "$archipelago_protocol$archipelago_domain"
 json_api_endpoint="$archipelago_protocol$archipelago_domain/jsonapi/metadatadisplay_entity/metadatadisplay_entity"
 
-if [ $selected_metadata_action -eq 1 ]
+if [ "${selected_metadata_action}" -eq 1 ]
 then
   export_source=""
   echo 'Export to the current directory? '
@@ -98,21 +98,21 @@ then
   select opt in "Yes, export from current directory." "No, specify a directory."; do
     case "$opt" in
       Yes*)
-	export_source=$PWD
+	export_source="$PWD"
         break;;
       No*)
 	      echo "Please provide an absolute directory (will be created if it doesn't already exist): "
-        read export_source
+        read -p "> " export_source
         break;;
       *) echo "Please select an option.";;
     esac
   done
   [ ! -d "$export_source" ] && mkdir -p "$export_source"
   echo "Exporting Metadata Displays to to the current directory."
-  cd $export_source &&
-  curl -w "\n" --user $username:$password -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XGET $json_api_endpoint | jq -cr '.data[]|del(.links)|del(.relationships)|del(.attributes.created)|del(.attributes.drupal_internal__id)|del(.attributes.changed)|del(.attributes.link)|{"data": .}'| awk -F'\t' '{ i++ ; fname = "metadatadisplay_entity_" i ".json"; print > fname; close(fname)}'
+  cd "$export_source" &&
+  curl -w "\n" --user "$username":"$password" -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XGET "$json_api_endpoint" | jq -cr '.data[]|del(.links)|del(.relationships)|del(.attributes.created)|del(.attributes.drupal_internal__id)|del(.attributes.changed)|del(.attributes.link)|{"data": .}'| awk -F'\t' '{ i++ ; fname = "metadatadisplay_entity_" i ".json"; print > fname; close(fname)}'
 
-elif [ $selected_metadata_action -eq 0 ]
+elif [ "${selected_metadata_action}" -eq 0 ]
 then
   import_source=""
   echo 'Import from the current directory? '
@@ -125,7 +125,7 @@ then
         break;;
       No*)
 	echo "Please provide an absolute directory: "
-        read import_source
+        read -p "> " import_source
         break;;
       *) echo "Please select an option.";;
     esac
@@ -135,14 +135,14 @@ then
   for f in $import_source*.json
   do
     import_id=$(jq -cr .data.id "$f")
-    import_id_exists=$(curl -I -w "\n" --user $username:$password --write-out '%{http_code}' --silent --output /dev/null $json_api_endpoint/$import_id)
+    import_id_exists=$(curl -I -w "\n" --user "$username":"$password" --write-out '%{http_code}' --silent --output /dev/null "$json_api_endpoint"/"$import_id")
     if [ $import_id_exists -eq 200 ]
     then
       echo 'Metadata Display Entity exists. Updating.'
-      curl -w "\n" --user $username:$password -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPATCH $json_api_endpoint/$import_id --data-binary @$f;
+      curl -w "\n" --user "$username":"$password" -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPATCH "$json_api_endpoint"/"$import_id" --data-binary @$f;
     else
       echo 'Metadata Display Entity exists. Importing.'
-      curl -w "\n" --user $username:$password -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPOST $json_api_endpoint --data-binary @$f;
+      curl -w "\n" --user "$username":"$password" -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPOST "$json_api_endpoint" --data-binary @"$f";
     fi
   done
 fi
