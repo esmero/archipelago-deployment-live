f3c11a21c517c1c43d16971c56ae4776e20f6578
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2026-06-17 18:34:25 -0400

    Deploy and allow updates on deployed for new Theme Switcher block

--- drupal/scripts/archipelago/update_deployed.sh
+++ drupal/scripts/archipelago/update_deployed.sh
@@ -116,6 +116,10 @@ echo -e "Advanced Search Page Block\n"
 
 curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPATCH http://esmero-web/jsonapi/block_content/block_with_twig_templating_/9aa72fb1-2817-44a7-8fb5-a3eb51166e83 --data-binary @d8content/block_content_02.json;
 
+echo -e "The Theme Mode Switcher Block\n"
+
+curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPATCH http://esmero-web/jsonapi/block_content/basic/18fa77c1-8f18-408a-b300-03fb05be8118 --data-binary @d8content/block_content_04.json;
+
 echo -e "Last pass, setting web owner to tmp files folder and clearing Drupal caches\n"
 
 chown -R www-data:www-data /var/www/html/web/sites/default/files
